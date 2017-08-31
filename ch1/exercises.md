7b65f6986755865b591b26b580830ea461749fe4
Cauchy Cai <cauchy.cai@gmail.com>
2017-08-31 19:24:27 +0800

    Finishing Chapter 1

---
+++ ch1/exercises.md
@@ -0,0 +1,19 @@
+# Exercises of Chapter 1
+
+* 1.2
+
+The 2 processes exited normally.
+
+* 1.3 ?
+
+Because the string pointer argument passed to perror is not allowed to be modified.
+
+* 1.4
+
+2^31/3600 => about 596 days. So it would have been flowed in 1971.
+?
+?
+
+* 1.5
+
+2^31/100/3600/24 => about 248 days
---
+++ ch1/sample-catch-signal.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXLINE 4096
+
+static void sig_int(int);       /* signal-catching function */
+
+int
+main(void)
+{
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
+
+    if (signal(SIGINT, sig_int) == SIG_ERR) {
+        printf("signal error");
+        exit(1);
+    }
+
+    printf("%% ");
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf) - 1] == '\n') {
+            buf[strlen(buf) - 1] = 0; /* replace new line with null */
+        }
+
+        if ((pid = fork()) < 0) {
+            printf("fork error");
+            exit(1);
+        } else if (pid == 0) { /* child */
+            execlp(buf, buf, (char *)0);
+            printf("couldn't execute: %s", buf);
+            exit(127);
+        }
+
+        /* parent */
+        if ((pid = waitpid(pid, &status, 0)) < 0) {
+            printf("waitpid error");
+            exit(1);
+        }
+
+        printf("%% ");
+    }
+
+    exit(0);
+}
+
+void
+sig_int(int signo)
+{
+    printf("interrupt with signal %d\r\n", signo);
+}
